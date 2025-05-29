70ba804a88c4e4722cbbec24bd58531668b77e09
munir45 <memamy3@uw.edu>
2025-05-28 19:08:54 -0700

    init

---
+++ PicturePerfect/PicturePerfect/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PicturePerfect
+//
+//  Created by Munir Emam on 5/28/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PicturePerfect/PicturePerfectTests/PicturePerfectTests.swift
@@ -0,0 +1,17 @@
+//
+//  PicturePerfectTests.swift
+//  PicturePerfectTests
+//
+//  Created by Munir Emam on 5/28/25.
+//
+
+import Testing
+@testable import PicturePerfect
+
+struct PicturePerfectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
