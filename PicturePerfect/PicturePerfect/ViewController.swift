022ed5d27f4ff30aed3e1415b3943d155e62737f
munir45 <memamy3@uw.edu>
2025-06-01 15:18:52 -0700

    searching implemented

--- PicturePerfect/PicturePerfect/ViewController.swift
+++ PicturePerfect/PicturePerfect/ViewController.swift
@@ -8,9 +8,26 @@
 import UIKit
 
 
-class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UITextFieldDelegate {
+    
+    @IBOutlet weak var MovieCollection: UICollectionView!
+    
+    @IBOutlet weak var catalogTab: UITabBarItem!
+    
+    @IBOutlet weak var searchField: UITextField!
+    
+    var popularMovies: [[String: Any]] = []
+    
+    var searchResult: [[String: Any]] = []
+    
+    var searching: Bool = false
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return popularMovies.count
+        if searching {
+            return searchResult.count
+        } else {
+            return popularMovies.count
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -18,7 +35,12 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
             // Return a default cell if the cast fails
             return UICollectionViewCell()
         }
-        let movie = popularMovies[indexPath.item]
+        var movie : [String:Any] = [:]
+        if searching {
+            movie = searchResult[indexPath.item]
+        } else {
+            movie = popularMovies[indexPath.item]
+        }
         if let posterPath = movie["poster_path"] as? String {
             if let imageURL = URL(string: "https://image.tmdb.org/t/p/w500\(posterPath)") {
                 URLSession.shared.dataTask(with: imageURL) {
@@ -38,17 +60,81 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     }
     
     
-
-    @IBOutlet weak var MovieCollection: UICollectionView!
-    
-    var popularMovies: [[String: Any]] = []
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         MovieCollection.dataSource = self
         MovieCollection.delegate = self
+        searchField.delegate = self
         fetchPopularMovies()
+    }
+    
+   
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+        textField.resignFirstResponder()
+        if let term = textField.text, !term.isEmpty {
+            fetchSearchMovies(search: term)
+            searching = true
+        } else {
+            searching = false
+            MovieCollection.reloadData()
+        }
+        return true
+    }
+    
+    func fetchSearchMovies(search : String) {
+        guard let secret = Bundle.main.object(forInfoDictionaryKey: "SECRET") as? String else {
+            print("NO KEY FOUND")
+            return
+        }
+        guard let url = URL(string: "https://api.themoviedb.org/3/search/movie") else {
+            return
+        }
+        
+        var components = URLComponents(url: url, resolvingAgainstBaseURL: true)!
+        let queryItems: [URLQueryItem] = [
+          URLQueryItem(name: "query", value: search),
+          URLQueryItem(name: "include_adult", value: "false"),
+          URLQueryItem(name: "language", value: "en-US"),
+          URLQueryItem(name: "page", value: "1"),
+        ]
+        
+        components.queryItems = components.queryItems.map { $0 + queryItems } ?? queryItems
+
+        var request = URLRequest(url: components.url!)
+        request.httpMethod = "GET"
+        request.timeoutInterval = 10
+        request.allHTTPHeaderFields = [
+          "accept": "application/json",
+          "Authorization": secret
+        ]
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            
+            guard let data = data else {
+                print("No data received")
+                return
+            }
+
+            
+            do {
+                guard let jsonObject = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] else {
+                    print("Could not cast JSON object as a Dictionary.")
+                    return
+                }
+                
+                self.searchResult = jsonObject["results"] as! [[String : Any]]
+                
+                DispatchQueue.main.async {
+                    self.MovieCollection.reloadData()
+                }
+                print(self.searchResult)
+            } catch {
+                print("cant parse popular movies")
+            }
+            
+        }.resume()
+
     }
     
     func fetchPopularMovies() {
