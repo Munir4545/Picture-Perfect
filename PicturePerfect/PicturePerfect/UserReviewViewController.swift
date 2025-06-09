e93bf83de91d41308fa51a7698bf1b68435ea848
vvaibava <vvaibava@gmail.com>
2025-06-08 17:51:37 -0700

    Added user profile reviews

---
+++ PicturePerfect/PicturePerfect/UserReviewViewController.swift
@@ -0,0 +1,29 @@
+//
+//  UserReviewViewController.swift
+//  PicturePerfect
+//
+//  Created by Vaibava Venkatesan on 6/8/25.
+//
+
+import UIKit
+
+class UserReviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
